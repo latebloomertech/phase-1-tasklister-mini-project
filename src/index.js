20536ac2c46d09cbd0d35129a28e85ffd7e512c3
Dan Baechtold <latebloomertech@gmail.com>
2022-01-18 12:14:07 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// const init = () => {
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target.elements[0].value)
+    console.log(e.target.elements[0].value)
+    form.reset()
+  })
+// }
+
+function buildToDo(todo) {
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  li.textContent = `${todo} `
+  li.appendChild(btn)
+  console.log(li)
+  document.getElementById('tasks').appendChild(li)
+}
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
+// document.addEventListener('DOMContentLoaded', init);
